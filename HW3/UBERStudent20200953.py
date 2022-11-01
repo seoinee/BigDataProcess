830159a47b7dc9d2510df199df211698d28787c5
seoinee <se001120@naver.com>
2022-11-01 16:47:29 +0900

    add hw3 code

--- HW3/UBERStudent20200953.py
+++ HW3/UBERStudent20200953.py
@@ -16,11 +16,17 @@ output = open(b, "wt")
 
 cnt = 0
 cnt2 = 0
+vehicles = dict()
+trips = dict()
+bind = dict()
+
 for line in f:
 	cnt = 0
 	for s in line.split(","):
 		cnt += 1
-		if cnt == 2:
+		if cnt == 1:
+			first = s
+		elif cnt == 2:
 			cnt2 = 0
 			for d in s.split("/"):
 				cnt2 += 1
@@ -31,6 +37,25 @@ for line in f:
 				else:
 					year = int(d)
 			weekday = what_day(datetime(year, month, day))
-			line = line.replace(s, weekday)
-			output.write(line)
+			first = first + "," + weekday
+		elif cnt == 3:
+			if first not in vehicles:
+				vehicles[first] = int(s)
+			else:
+				vehicles[first] += int(s)
+		else:
+			if first not in trips:
+				trips[first] = int(s)
+			else:
+				trips[first] += int(s)
+for key in vehicles:
+	if key not in bind:
+		bind[key] = str(vehicles[key]) + "," + str(trips[key])
+strBind = ""
+
+for key, value in bind.items():
+	strBind += key + ' '
+	strBind += value + '\n'
+output.write(strBind)
+
 output.close()
